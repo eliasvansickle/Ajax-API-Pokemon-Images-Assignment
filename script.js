d90c420f46af99d2ebee2e5c1c8f76d0d61c41b5
eliasvansickle <eliasvansickle@gmail.com>
2015-06-09 12:33:50 -0700

    pokemon images working well

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 $(document).ready(function() {
-
+	var imgUrl = function() {
+	for (var i = 1; i < 152; i++) {
+		$("#wrapper").append('<img src="http://pokeapi.co/media/img/'+i+'.png">');
+	}
+};
+imgUrl();
 });
